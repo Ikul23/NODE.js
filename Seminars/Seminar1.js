6ab94f58edd094a607ec1b3626765e25b374a61c
Ikul23 <ikul23@icloud.com>
2024-12-05 20:02:37 +0300

    HT complited

---
+++ Seminars/Seminar1.js
@@ -0,0 +1,28 @@
+// Написание HTTP-сервера
+const http = require('http');
+
+let clickCount = 0;
+
+const server = http.createServer((req, res) => {
+
+clickCount++;
+  // Проверяем путь запроса
+  if (req.url === '/') {
+    res.writeHead(200, { 'content-type': 'text/html; charset=utf-8' });    
+    res.end(`<p>Количество кликов: ${clickCount}</p>
+      <a href="/about">Перейти на страницу обо мне</a>`);
+  } else if (req.url === '/about') {
+    res.writeHead(200, { 'content-type': 'text/html; charset=utf-8' });
+    res.end(`<p>Количество кликов: ${clickCount}</p>
+      <a href="/">Перейти на главную страницу</a>`);
+  } else {
+    res.writeHead(404, { 'content-type': 'text/html; charset=utf-8' });
+    res.end('<h1>СТРАНИЦА НЕ НАЙДЕНА!!!</h1>');
+  }
+});
+
+// Указываем порт для сервера
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Сервер запущен на http://localhost:${port}`);
+});
